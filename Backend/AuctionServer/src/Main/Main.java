d20a5036dc5414d88a027c788083d852d2e95f29
Declan Rhodes <declanrhodesss@gmail.com>
2022-02-15 13:44:53 +0000

    feat: got mocking working finally

--- Backend/AuctionServer/src/Main/Main.java
+++ Backend/AuctionServer/src/Main/Main.java
@@ -2,12 +2,12 @@ package Main;
 import Handlers.AuctionHandler;
 import Models.Auction;
 import Repository.AuctionRepository;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,8 +21,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(9090);
 
+
         //TEMP
-        AuctionRepository auctionRepository = new AuctionRepository();
+        AuctionRepository auctionRepository = Mockito.mock(AuctionRepository.class);
+        //AuctionList config
+        ArrayList<Auction> aucList = new ArrayList<>();
+        aucList.add(new Auction("Temp", 1, 10,1));
+        aucList.add(new Auction("Auction for Stuff",10,100,5));
+        //GetAuctionByName mocks
+        Mockito.when(auctionRepository.getAuctionByName("Temp")).thenReturn(aucList.get(0));
+        Mockito.when(auctionRepository.getAuctionByName(("Auction for Stuff"))).thenReturn(aucList.get(1));
+        //GetAllAuctionMock
+        Mockito.when(auctionRepository.getAllAuctions()).thenReturn(aucList);
 
         while(true){
             System.out.println("Waiting for client connection...");
@@ -32,7 +42,7 @@ public class Main {
             System.out.println("Connected to client!");
 
             //Get Auction They want to Connect to
-            var auctionGet = auctionRepository.getAuctionByName("temp");
+            var auctionGet = auctionRepository.getAuctionByName("Temp");
 
             //adds new auctionThread to auctionHandlers list
             AuctionHandler auctionThread = new AuctionHandler(client, auctionHandlers, auctionGet, auctionRepository);
